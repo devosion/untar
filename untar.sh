817cff29e1c4e6d916ed931723b1644e25b57334
Lorenzo Salazar <devosion@gmail.com>
2016-11-14 09:55:28 -0700

    Some minor fixes.

--- untar.sh
+++ untar.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # a script for detecting and unpacking tar archives
 
+
+# display usage
+DisplayUsage () {
+
+    printf "USAGE: untar.sh [tar_file]
+Currently supports .tar.gz, .tar.bz2, and .tar.xz.\n"
+}
+
 # processes the file argument and unpacks it via tar and
 # the compression type
 ProcessArchive() {
@@ -19,24 +27,24 @@ ProcessArchive() {
     fi
 
     # check for empty string, or .tar file
-    if [ -z $tar_type ]; then
+    if [[ -z $tar_type ]]; then
 
         tar xvf ${tar_file}
 
     # for .tar.gz files
-    elif [ $tar_type == "gz" ]; then
+    elif [[ $tar_type == "gz" ]]; then
 
         option="z"
         tar xvf${option} ${tar_file}
 
     # for .tar.bz2 files
-    elif [ $tar_type == "bz2" ]; then
+    elif [[ $tar_type == "bz2" ]]; then
 
         option="j"
         tar xvf${option} ${tar_file}
 
     # for tar.xz files
-    elif [ $tar_type == "xz" ]; then
+    elif [[ $tar_type == "xz" ]]; then
         
         option="J"
         tar xvf${option} ${tar_file}
@@ -47,6 +55,12 @@ ProcessArchive() {
     fi
 
 }
+
+# if no passed arguments then display usage and exit
+if [[ -z $1 ]]; then
+    DisplayUsage 
+    exit 1
+fi
 
 ProcessArchive $1
 
